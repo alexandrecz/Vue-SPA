3650c663e121551de3d477978e646bcafc4d190d
Czechowicz <Alexandre_Czechowicz@DellTeam.com>
2018-07-31 17:00:57 -0300

    add Vuex logic

--- spa/src/components/Products.vue
+++ spa/src/components/Products.vue
@@ -1,11 +1,25 @@
 <template>
-  <div>
-
-  </div>
+  <ul>
+    <li v-for="product in products" v-bind:key="product.id">
+     {{ product.title}} - {{ product.price}}
+    </li>
+  </ul>
 </template>
 <script>
+  import {mapState, mapActions} from 'vuex'
+
   export default {
-    name: 'Products'
+    name: 'Products',
+
+    computed: mapState({
+      products: state => state.products.all
+    }),
+    methods: mapActions('cart',[
+      'addProductToCart'
+    ]),
+    created(){
+      this.$store.dispatch('products/getAllProducts')
+    }
   }
 </script>
 <style>
--- spa/src/store/index.js
+++ spa/src/store/index.js
@@ -4,9 +4,9 @@
 
 import Vue from 'vue'
 import Vuex from 'vuex'
-import cart from '.modules/cart'
-import products from '.modules/products'
-import createLogger from '../../../src/plugins/logger'
+import cart from './modules/cart'
+import products from './modules/products'
+// import createLogger from '../../../src/plugins/logger'
 
 Vue.use(Vuex)
 
@@ -18,5 +18,5 @@ export default new Vuex.Store({
     products
   },
   strict: debug,
-  plugins: debug ? [createLogger()] : []
+  plugins: debug ? [] : []
 })
